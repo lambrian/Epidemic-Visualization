f32e71dc70d4d1ee7e167b7b1a1ca9c20c6b131b
Brian Lam <brianlam@stanford.edu>
2015-05-11 00:40:45 -0700

    added sir chart

--- js/main.js
+++ js/main.js
@@ -2,16 +2,38 @@ $(function() {
   var network = loadInitialNetwork(120);
 });
 
+function updateSIR() {
+
+  statusCount = {};
+  statusCount['susceptible'] = 0;
+  statusCount['infected'] = 0;
+  statusCount['dead'] = 0;
+
+  cy.nodes().forEach(function(u) {
+    statusCount[u.data('status')]++;
+  });
+
+  var numNodes = cy.nodes().length;
+  $("#sir-susceptible").height(statusCount['susceptible'] / numNodes * 100 + '%');
+  $("#sir-infected").height(statusCount['infected'] / numNodes * 100 + '%');
+  $("#sir-dead").height(statusCount['dead'] / numNodes * 100 + '%');
+
+}
+
 $("#startSimulate").click(function() {
   beginSimulation($("#infection-rate").val(),
            $("#contagious").val(),
            $("#mortality-rate").val(),
            $("#initial-infected").val()
            );
+
+  sirChart = setInterval(function () { updateSIR(); }, 1000);
 });
 
 $("#reset").click(function() {
   reset(cy);
+  clearInterval(sirChart);
+  updateSIR();
 })
 
 
